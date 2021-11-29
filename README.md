97ff11d855c508c56a07aed59753b55330ca1ac5
Daniel McKnight <daniel@neongecko.com>
2021-11-29 14:50:12 -0800

    Initial implementation of Neon skill rebased on upstream

--- README.md
+++ README.md
@@ -1,23 +1,89 @@
-# <img src='https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/question.svg' card_color='#22a7f0' width='50' height='50' style='vertical-align:bottom'/> Unknown Handler
-Capture unrecognized _Utterances_
+# <img src='https://0000.us/klatchat/app/files/neon_images/icons/neon_skill.png' card_color="#FF8600" width="50" style="vertical-align:bottom">fallback-unknown
 
-## About
-Mycroft doesn't know how to do or answer everything (yet).  This _fallback_ is how Mycroft lets you know that, unfortunately, it can't help with what you said.
+## Summary
 
-But wait, there is still hope!  Mycroft is working to get smarter with help from friends.  For who have selected to [Opt In to the Open Dataset](https://home.mycroft.ai/#/setting/basic#opendataset), these missed phrases are aggregated and analyzed to help identify what the world _wants_ their voice assistant to do that it can't yet.  So if millions of people start asking for guava growing tips, we'll promote this as an idea to be handled as a default Skill.
+Unknown request fallback handler. Executes if every other step failed to answer the question.
 
-## Examples
-* "How do I make my guava blue?"
-* "I need a pizza"
-* "Show me the money"
+## Requirements
+
+No special required packages for this skill.
+
+## Description
+
+This fallback is how Neon would let you know if he can't help with what you said and answer your question. This skill will execute as a last resort, and only if you are currently in the wakewords-required mode. If you are skipping wakewords, the failed utterances will be ignored. Neon will try to match the request to Adapt skills, Padatious skills, and all of Fallbacks before finally l reaching it here.
+
+## Location
+
+    ${skills}/fallback-unknown.neon
+
+## Files
+<details>
+<summary>Click to expand.</summary>
+<br>
+
+    ${skills}/fallback-unknown.neon/__init__.py  
+    ${skills}/fallback-unknown.neon/.gitignore  
+    ${skills}/fallback-unknown.neon/settings.json  
+    ${skills}/fallback-unknown.neon/dialog  
+    ${skills}/fallback-unknown.neon/dialog/es-es  
+    ${skills}/fallback-unknown.neon/dialog/es-es/who.is.dialog  
+    ${skills}/fallback-unknown.neon/dialog/es-es/unknown.dialog  
+    ${skills}/fallback-unknown.neon/dialog/es-es/why.dialog  
+    ${skills}/fallback-unknown.neon/dialog/es-es/question.dialog  
+    ${skills}/fallback-unknown.neon/dialog/sv-se  
+    ${skills}/fallback-unknown.neon/dialog/sv-se/who.is.dialog  
+    ${skills}/fallback-unknown.neon/dialog/sv-se/unknown.dialog  
+    ${skills}/fallback-unknown.neon/dialog/sv-se/why.dialog  
+    ${skills}/fallback-unknown.neon/dialog/sv-se/question.dialog  
+    ${skills}/fallback-unknown.neon/dialog/en-us  
+    ${skills}/fallback-unknown.neon/dialog/en-us/who.is.dialog  
+    ${skills}/fallback-unknown.neon/dialog/en-us/unknown.dialog  
+    ${skills}/fallback-unknown.neon/dialog/en-us/why.dialog  
+    ${skills}/fallback-unknown.neon/dialog/en-us/question.dialog  
+    ${skills}/fallback-unknown.neon/dialog/de-de  
+    ${skills}/fallback-unknown.neon/dialog/de-de/who.is.dialog  
+    ${skills}/fallback-unknown.neon/dialog/de-de/unknown.dialog  
+    ${skills}/fallback-unknown.neon/dialog/de-de/why.dialog  
+    ${skills}/fallback-unknown.neon/dialog/de-de/question.dialog  
+    ${skills}/fallback-unknown.neon/vocab  
+    ${skills}/fallback-unknown.neon/vocab/es-es  
+    ${skills}/fallback-unknown.neon/vocab/es-es/who.is.voc  
+    ${skills}/fallback-unknown.neon/vocab/es-es/why.is.voc  
+    ${skills}/fallback-unknown.neon/vocab/es-es/question.voc  
+    ${skills}/fallback-unknown.neon/vocab/sv-se  
+    ${skills}/fallback-unknown.neon/vocab/sv-se/who.is.voc  
+    ${skills}/fallback-unknown.neon/vocab/sv-se/why.is.voc  
+    ${skills}/fallback-unknown.neon/vocab/sv-se/question.voc  
+    ${skills}/fallback-unknown.neon/vocab/en-us  
+    ${skills}/fallback-unknown.neon/vocab/en-us/who.is.voc  
+    ${skills}/fallback-unknown.neon/vocab/en-us/why.is.voc  
+    ${skills}/fallback-unknown.neon/vocab/en-us/question.voc  
+    ${skills}/fallback-unknown.neon/vocab/de-de  
+    ${skills}/fallback-unknown.neon/vocab/de-de/who.is.voc  
+    ${skills}/fallback-unknown.neon/vocab/de-de/why.is.voc  
+    ${skills}/fallback-unknown.neon/vocab/de-de/question.voc  
+    ${skills}/fallback-unknown.neon/LICENSE  
+    ${skills}/fallback-unknown.neon/README.md
+
+  </details>
+
+## Class Diagram
+
+[Click Here](https://0000.us/klatchat/app/files/neon_images/class_diagrams/fallback-unknown.png)
+
+  
+
+## Troubleshooting
+
+Check your signal handling in `/mycroft/ipc` if you are receiving response from this skill while skipping wake words.
+
+  
+
+## Contact Support
+
+Use the [link](https://neongecko.com/ContactUs) or [submit an issue on GitHub](https://help.github.com/en/articles/creating-an-issue)
 
 ## Credits
-Mycroft AI (@MycroftAI)
-
-## Category
-**Configuration**
-
-## Tags
-#fallback
-#unknown
-#system
+[Mycroft AI](https://github.com/MycroftAI)
+[NeonDaniel](https://github.com/NeonDaniel)
+[reginaneon](https://github.com/reginaneon)
--- __init__.py
+++ __init__.py
@@ -1,3 +1,31 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2022 Neongecko.com Inc.
+# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
+# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
+# BSD-3 License
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 # Copyright 2017 Mycroft AI, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,10 +40,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from mycroft.skills.core import FallbackSkill
+from neon_utils.message_utils import request_from_mobile
+from neon_utils.skills.neon_fallback_skill import NeonFallbackSkill
+from neon_utils import LOG
 
 
-class UnknownSkill(FallbackSkill):
+class UnknownSkill(NeonFallbackSkill):
     def __init__(self):
         super(UnknownSkill, self).__init__()
 
@@ -27,21 +57,46 @@ class UnknownSkill(FallbackSkill):
             return filter(bool, map(str.strip, f.read().split('\n')))
 
     def handle_fallback(self, message):
-        utterance = message.data['utterance'].lower()
+        LOG.debug("Unknown Fallback Checking for Neon!!!")
+        # This checks if we're pretty sure this was a request intended for Neon
+        if not hasattr(self, "neon_in_request") or self.neon_in_request(message):
+            utterance = message.data['utterance']
 
-        try:
-            self.report_metric('failed-intent', {'utterance': utterance})
-        except Exception:
-            self.log.exception('Error reporting metric')
+            try:
+                self.report_metric('failed-intent', {'utterance': utterance,
+                                                     'device': self.local_config["devVars"]["devType"]})
+            except Exception as e:
+                LOG.error(e)
+                self.log.exception('Error reporting metric')
 
-        for i in ['question', 'who.is', 'why.is']:
-            for l in self.read_voc_lines(i):
-                if utterance.startswith(l):
-                    self.log.info('Fallback type: ' + i)
-                    self.speak_dialog(i, data={'remaining': l.replace(i, '')})
+            if len(utterance.split()) >= 2:
+                if hasattr(self, "server") and self.server:
+                    LOG.debug(f"Checking if neon must respond: {message.data}")
+                    if hasattr(self, "neon_must_respond") and self.neon_must_respond(message):
+                        if request_from_mobile(message):
+                            self.speak_dialog("websearch")
+                            self.mobile_skill_intent("web_search", {"term": message.data.get('utterance')}, message)
+                            # self.socket_io_emit("web_search", f"&term={message.data.get('utterance')}",
+                            #                     message.context["flac_filename"])
+                        # TODO: Handle server web results here DM
+                else:
+                    for i in ['question', 'who.is', 'why.is']:
+                        for l in self.read_voc_lines(i):
+                            if utterance.startswith(l):
+                                self.log.info('Fallback type: ' + i)
+                                if not self.check_for_signal("SKILLS_useDefaultResponses", -1):
+                                    self.speak_dialog(i, data={'remaining': l.replace(i, '')})
+                                else:
+                                    self.speak("I'm not sure how to help you with that.")
+                                return True
+                    if self.local_config.get("interface", {}).get("wake_word_enabled", True):
+                        if not self.check_for_signal("SKILLS_useDefaultResponses", -1):
+                            self.speak_dialog('unknown')
+                        elif self.check_for_signal("SKILLS_useDefaultResponses", -1):
+                            self.speak("I'm not sure how to help you with that.")
                     return True
-        self.speak_dialog('unknown')
-        return True
+        # else:
+        #     self.check_for_signal("CORE_andCase")
 
 
 def create_skill():
